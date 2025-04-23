77b715c416a8f1a643a6648119347588f9ec9650
harfool <harfoolgujjar63@gmail.com>
2025-04-23 08:48:54 +0530

    assests import and Aboutpage setup

--- lms client/src/App.jsx
+++ lms client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
+import AboutUs from './pages/AboutPage'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
 
      <Routes>
       <Route path='/' element={<HomePage/>} />
+      <Route path='/about' element={<AboutUs/>}  />
      </Routes>
     </>
   )
--- lms client/src/components/Footer.jsx
+++ lms client/src/components/Footer.jsx
@@ -13,23 +13,23 @@ const FooterComponent = () => {
 
   return (
     <footer className="relative left-0 bottom-0 h-[10vh] flex flex-col sm:flex-row justify-between items-center text-white bg-gray-800 py-6 px-4 sm:px-20">
-      <section className="text-lg mb-2 sm:mb-0">
+      <section className="mb-2 text-lg sm:mb-0">
         © {year} | All rights reserved
       </section>
-      <div className="socialMediaIcons flex items-center justify-center gap-5 text-2xl">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-500 transition-all duration-300">
+      <div className="flex items-center justify-center gap-5 text-2xl socialMediaIcons">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 hover:text-yellow-500">
           <BsFacebook />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-500 transition-all duration-300">
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 hover:text-yellow-500">
           <BsInstagram />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-500 transition-all duration-300">
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 hover:text-yellow-500">
           <BsLinkedin />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-500 transition-all duration-300">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 hover:text-yellow-500">
           <BsTwitter />
         </a>
-        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-500 transition-all duration-300">
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 hover:text-yellow-500">
           <BsYoutube />
         </a>
       </div>
